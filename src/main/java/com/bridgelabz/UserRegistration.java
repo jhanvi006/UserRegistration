ac38f2b0f071043663314c22390f84dbe772fce3
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-09 16:04:04 -0400

    Added: validation for first name is added.

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration page!");
+    public static final Scanner sc = new Scanner(System.in);
+    public static boolean validFirstName() {
+        System.out.print("Enter the First Name : ");
+        String name = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean r = m.matches();
+
+        if (r)
+            System.out.println("First name is valid.");
+        else
+            System.out.println("First name is invalid.");
+        return r;
     }
 }
